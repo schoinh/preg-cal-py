6c3bd36d4296a15fa7319c91dc7b4ea49ae5cf41
Na Hyung Choi <schoinh@gmail.com>
2024-12-22 16:31:01 -0800

    gitignore

--- cli.py
+++ cli.py
@@ -1,6 +1,10 @@
 import datetime
+import random
 
 import click
+
+def format_date(date_object: datetime.date) -> str:
+    return date_object.strftime("%Y%m%d")
 
 def get_week_bounds(due_date: datetime.date):
     week_bounds = []
@@ -9,7 +13,7 @@ def get_week_bounds(due_date: datetime.date):
     week_end = week_start + datetime.timedelta(days=7)
 
     while week_num <= 42:
-        week = (week_num, week_start.strftime("%Y%m%d"), week_end.strftime("%Y%m%d"))
+        week = (week_num, format_date(week_start), format_date(week_end))
         week_bounds.append(week)
 
         week_num += 1
@@ -22,26 +26,29 @@ def get_week_events(week_bounds):
     week_events = ""
 
     for (week_num, week_start, week_end) in week_bounds:
-        week_event = (
-            "BEGIN:VEVENT\n"
-            "CREATED:20241222T231056Z\n"
-            f"DTEND;VALUE=DATE:{week_end}\n"
-            "DTSTAMP:20241222T231407Z\n"
-            f"DTSTART;VALUE=DATE:{week_start}\n"
-            "LAST-MODIFIED:20241222T231124Z\n"
-            "SEQUENCE:1\n"
-            f"SUMMARY:{week_num}W\n"
-            "TRANSP:TRANSPARENT\n"
-            f"UID:PREG-CAL-{week_num}\n"
-            "BEGIN:VALARM\n"
-            "ACTION:NONE\n"
-            "TRIGGER;VALUE=DATE-TIME:19760401T005545Z\n"
-            "END:VALARM\n"
-            "END:VEVENT\n"
-        )
-        week_events += week_event
+        week_events += create_event(f"{week_num}W", week_start, week_end)
     
     return week_events
+
+def create_event(name, start, end):
+    hex = "".join([random.choice('0123456789abcdef') for i in range(6)])
+    return (
+        "BEGIN:VEVENT\n"
+        "CREATED:20241222T231056Z\n"
+        f"DTEND;VALUE=DATE:{end}\n"
+        "DTSTAMP:20241222T231407Z\n"
+        f"DTSTART;VALUE=DATE:{start}\n"
+        "LAST-MODIFIED:20241222T231124Z\n"
+        "SEQUENCE:1\n"
+        f"SUMMARY:{name}\n"
+        "TRANSP:TRANSPARENT\n"
+        f"UID:PREG-CAL-{name}-{hex}\n"
+        "BEGIN:VALARM\n"
+        "ACTION:NONE\n"
+        "TRIGGER;VALUE=DATE-TIME:19760401T005545Z\n"
+        "END:VALARM\n"
+        "END:VEVENT\n"
+    )
 
 @click.command()
 @click.option("--year", prompt="In what year are you due? Ex) 2025")
@@ -50,6 +57,9 @@ def get_week_events(week_bounds):
 def preg_call(year, month, date):
     click.confirm(f"Sounds like your due date is {month}/{date}/{year}?")
     due_date = datetime.date(int(year), int(month), int(date))
+    due_date_event = create_event(
+        "Due Date", format_date(due_date), format_date(due_date + datetime.timedelta(days=1))
+    )
 
     header = (
         "BEGIN:VCALENDAR\n"
@@ -62,10 +72,10 @@ def preg_call(year, month, date):
     footer = "END:VCALENDAR"
 
     week_bounds = get_week_bounds(due_date)
-    events = get_week_events(week_bounds)
+    week_events = get_week_events(week_bounds)
     
     with open("preg-cal.ics", "w") as file:
-        file.write(header + events + footer)
+        file.write(header + week_events + due_date_event + footer)
 
 if __name__ == '__main__':
     preg_call()
