2b27ca71f2eda8ee79cd96db166a389a0e9cc26b
Na Hyung Choi <schoinh@gmail.com>
2025-05-10 17:57:11 -0700

    Rearrange stuff

--- main.py
+++ main.py
@@ -13,23 +13,10 @@ def get():
         "Pregnancy Calendar Generator",
         Container(
             P(
-                "Enter your estimated due date to generate a calendar (.ics) file containing week-long events from 4 to 42 weeks."
+                "Enter your estimated due date to generate a calendar (.ics) file containing week-long events from 4 to 42 weeks:"
             ),
-            P(
-                "Import it as a new calendar into your Google Calendar, Apple Calendar, or Outlook, where you can:"
-            ),
-            Ul(
-                Li("View the week numbers alongside all your other life events"),
-                Li("Show/hide the calendar as needed"),
-                Li("Change the name, color, and events as you would with any other calendar layer"),
-            ),
-            P(
-                "It's your file once you've downloaded it. If your due date changes, you can just delete the calendar and generate a new one here."
-            ),
-            Hr(),
             Div(
                 Form(
-                    Label("Estimated Due Date:"),
                     Group(
                         Input(
                             type="date",
@@ -48,24 +35,35 @@ def get():
                     action="/generate",
                 ),
                 cls="grid",
-                style="max-width: 480px;",
+                style="margin-bottom: 20px; max-width: 480px;",
+            ),
+            P(
+                "Import the file as a new calendar into your Google Calendar, Apple Calendar, or Outlook, where you can:"
+            ),
+            Ul(
+                Li("View the week numbers alongside all your other life events"),
+                Li("Show/hide the calendar as needed"),
+                Li("Change the name, color, and events as you would with any other calendar layer"),
+            ),
+            Div(
+                Img(src="screenshot-google.png", width="800px", style="border: 2px solid black; margin-bottom: 2em;"),
+                id="screenshot-google",
+            ),
+            P(
+                "It's your file once you've downloaded it. If your due date changes, you can just delete the calendar and generate a new one here."
             ),
             Hr(),
             H3("How to import the .ics file into your calendar app"),
             P("Note: The exact steps can vary and change due to updates to calendar applications. If the directions below seem outdated, a quick web search should help you out."),
+            P("Google Calendar"),
+            Ol(
+                Li("Go to Settings > Add Calendar to create a new calendar. (Importing the file into a new calendar will make it easy for you to delete the calendar if you need to change the due date or just want to remove all the calendar events.)"),
+                Li("In Settings > Import & Export > Import, upload the .ics file, select the new calendar you created, and click Import."),
+            ),
             P("Apple Calendar"),
             Ol(
                 Li("Simply drag and drop the file onto your Calendar app."),
                 Li("When asked for a destination calendar, choose \"New Calendar...\" and give your new calendar a name."),
-            ),
-            Div(
-                Img(src="screenshot.png", width="500px", style="border: 2px solid black; margin-bottom: 2em;"),
-                id="screenshot",
-            ),
-            P("Google Calendar"),
-            Ol(
-                Li("Go to Settings > Add Calendar to create a new calendar."),
-                Li("In Settings > Import & Export > Import, upload the .ics file, select the new calendar you created, and click Import."),
             ),
             P("Outlook Calendar"),
             Ul(
