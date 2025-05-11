d38247f0008fbc0e2b4aac107a12e72ab750691d
Na Hyung Choi <schoinh@gmail.com>
2025-05-10 18:06:14 -0700

    boldface calendar app names

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def get():
                 Li("Change the name, color, and events as you would with any other calendar layer"),
             ),
             Div(
-                Img(src="screenshot-google.png", width="800px", style="border: 2px solid black; margin-bottom: 2em;"),
+                Img(src="screenshot-google.png", width="800px", style="border: 2px solid black; margin-bottom: 1em;"),
                 id="screenshot-google",
             ),
             P(
@@ -55,17 +55,17 @@ def get():
             Hr(),
             H3("How to import the .ics file into your calendar app"),
             P("Note: The exact steps can vary and change due to updates to calendar applications. If the directions below seem outdated, a quick web search should help you out."),
-            P("Google Calendar"),
+            P(B("Google Calendar")),
             Ol(
                 Li("Go to Settings > Add Calendar to create a new calendar. (Importing the file into a new calendar will make it easy for you to delete the calendar if you need to change the due date or just want to remove all the calendar events.)"),
                 Li("In Settings > Import & Export > Import, upload the .ics file, select the new calendar you created, and click Import."),
             ),
-            P("Apple Calendar"),
+            P(B("Apple Calendar")),
             Ol(
                 Li("Simply drag and drop the file onto your Calendar app."),
                 Li("When asked for a destination calendar, choose \"New Calendar...\" and give your new calendar a name."),
             ),
-            P("Outlook Calendar"),
+            P(B("Outlook Calendar")),
             Ul(
                 Li("The steps may vary based on your operating system (Mac vs. PC) and the platform you're using (web vs. desktop app). A web search is recommended for the most up-to-date information."),
             ),
