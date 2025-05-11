3493e78ce7e7f63eed82d8fa7d8312d4b1d380e5
Na Hyung Choi <schoinh@gmail.com>
2025-05-10 18:45:01 -0700

    More doc updates

--- main.py
+++ main.py
@@ -46,18 +46,23 @@ def get():
                 Li("Change the name, color, and events as you would with any other calendar layer"),
             ),
             Div(
-                Img(src="screenshot-google.png", width="800px", style="border: 2px solid black; margin-bottom: 1em;"),
+                Img(src="screenshot-google.png", width="800px", style="border: 1px solid gray; border-radius: 10px; box-shadow: 10px 10px 18px -1px rgba(0,0,0,0.27); margin-bottom: 1em;"),
                 id="screenshot-google",
-            ),
-            P(
-                "It's your file once you've downloaded it. If your due date changes, you can just delete the calendar and generate a new one here."
             ),
             Hr(),
             H3("How to import the .ics file into your calendar app"),
-            P("Note: The exact steps can vary and change due to updates to calendar applications. If the directions below seem outdated, a quick web search should help you out."),
+            Div(
+                H4("Tips"),
+                Ul(
+                    Li("The exact steps can vary and change due to updates to calendar applications. If the directions below seem outdated, a quick web search should help you out."),
+                    Li("Importing the file into a new calendar instead of an existing one will allow you to show and hide just the pregnancy calendar, give it its own color, and easily delete all calendar events for the pregnancy weeks if needed."),
+                    Li("If your due date changes, simply delete the calendar and generate a new one here."),
+                ),
+                style="border: 1px solid gray; border-radius: 10px; margin: 1em 0; padding: 1em;",
+            ),
             P(B("Google Calendar")),
             Ol(
-                Li("Go to Settings > Add Calendar to create a new calendar. (Importing the file into a new calendar will make it easy for you to delete the calendar if you need to change the due date or just want to remove all the calendar events.)"),
+                Li("Go to Settings > Add Calendar to create a new calendar."),
                 Li("In Settings > Import & Export > Import, upload the .ics file, select the new calendar you created, and click Import."),
             ),
             P(B("Apple Calendar")),
@@ -68,6 +73,18 @@ def get():
             P(B("Outlook Calendar")),
             Ul(
                 Li("The steps may vary based on your operating system (Mac vs. PC) and the platform you're using (web vs. desktop app). A web search is recommended for the most up-to-date information."),
+            ),
+            Hr(),
+            Div(
+                P(
+                    "Need help? Have feedback? Send an email to ",
+                    A("pregcal.help@gmail.com", href="mailto:pregcal.help@gmail.com"),
+                    " or file an issue on ",
+                    A("GitHub", href="https://github.com/schoinh/preg-cal-py"),
+                    "!",
+                ),
+                P("Made with ❤️ in Seattle"),
+                style="font-size: 0.8em; text-align: center;",
             ),
         ),
     )
